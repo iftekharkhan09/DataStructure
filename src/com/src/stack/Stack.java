4b139853a4562af83ab0137d2ffaf738ecb3c870
iftekharkhan09 <iftekharkhan629@gmail.com>
2017-04-20 00:08:35 +0530

    Committing the code for StackImplementation

---
+++ src/com/src/stack/Stack.java
@@ -0,0 +1,43 @@
+package com.src.stack;
+
+import java.util.Scanner;
+
+public class Stack {
+	public static void main(String[] args) {
+		String stack[] = new String[20];
+		int userInput;
+		int j = 0;
+		do {
+			System.out.println("------------------- Stack Implementation -----------------");
+			System.out.println("Please enter your choice");
+			System.out.println("1. Push Element into the Stack");
+			System.out.println("2. Pop Element out of the Stack");
+			System.out.println("3. Show the Element of the Stack");
+			String input = new Scanner(System.in).next();
+			userInput = new Integer(input);
+			switch (userInput) {
+			case 1:
+				System.out.println("Please enter the element to push");
+				String pushedElement = new Scanner(System.in).next();
+				stack[j] = pushedElement;
+				j++;
+				break;
+			case 2:
+				if (j == 0)
+					System.out.println("The Stack is Empty");
+				else {
+					System.out.println("The Poped Out Element is :" + stack[j - 1]);
+					stack[j - 1] = null;
+					j--;
+				}
+				break;
+			case 3:
+				System.out.println("The Element of the Stack is :");
+				for (String s : stack) {
+					if (s != null)
+						System.out.print(s + " ");
+				}
+			}
+		} while (userInput == 1 || userInput == 2 || userInput == 3);
+	}
+}
