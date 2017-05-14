00f018fdc308b7d59e4a7eb689989d0b0360a48a
iftekharkhan09 <iftekharkhan629@gmail.com>
2017-05-14 09:13:37 +0530

    formatted the code.

--- src/com/src/queue/PriorityQueueImpl.java
+++ src/com/src/queue/PriorityQueueImpl.java
@@ -1,13 +1,11 @@
 package com.src.queue;
-
 import java.util.Comparator;
 import java.util.PriorityQueue;
-
 public class PriorityQueueImpl<E> {
 	private PriorityQueue<E> queue;
 	private int size;
 	public PriorityQueueImpl() {
-		this.queue = new PriorityQueue<>(new QueueComparator());
+		this.queue = new PriorityQueue<E>(new QueueComparator<E>());
 	}
 	public static void main(String[] args) {
 		PriorityQueueImpl<Integer> priorityQueueImpl = new PriorityQueueImpl<>();
@@ -19,13 +17,11 @@ public class PriorityQueueImpl<E> {
 		priorityQueueImpl.remove();
 		priorityQueueImpl.show();
 	}
-
 	private boolean add(E e) {
 		queue.add(e);
 		this.size++;
 		return true;
 	}
-
 	private boolean remove() {
 		queue.remove();
 		this.size--;
@@ -36,7 +32,6 @@ public class PriorityQueueImpl<E> {
 		System.out.println(queue);
 	}
 }
-
 class QueueComparator<E> implements Comparator<E> {
 	@Override
 	public int compare(E arg0, E arg1) {
